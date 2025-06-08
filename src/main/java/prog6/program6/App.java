a36eed81cf552f0ab109259f20e8b7b69b6accbe
yashaswinib019 <saiyashaswini.22cse@cambridge.edu.in>
2025-06-08 12:43:39 +0530

    Add files via upload

---
+++ src/main/java/prog6/program6/App.java
@@ -0,0 +1,13 @@
+package prog6.program6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
